b9682dc144711626106c6e90532cd1c9daef3acb
Adrian Laje <67974071+adrianrd7@users.noreply.github.com>
2023-09-04 16:16:33 -0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -470,7 +470,7 @@ public:
         distMax = 0.0;
     }
 
-   
+
 
     double IntersectionDistance(vector1 n, point pnt, vector1 u, point o) {
         double m = n * u;
@@ -644,139 +644,122 @@ public:
 };
 //---------------------------------------------------------------------------
 class source {
-public:
-    point p;                //Posici n
-    vector1* Rays;           //Direcciones de partida de los rayos
-    int NRAYS;              //N mero de rayos
-    double eF;              //Energ a inicial de la fuente
-    triangle IcoFace[20];   //Representaci n gr fica de la fuente
-    double VisualRadius;    //Tama o de la fuente (radio visual)
+    public:
+        point p;                //Posici n
+        triangle IcoFace[20];   //Representaci n gr fica de la fuente
+        //color Color;            //Color de la fuente
+        double  VisualRadius;   //radio visual
+        double eF;              //Energ a de la fuente
+        int NRAYS;              //N mero de rayos que parten de la fuente
+        vector1* Rays;           //Direcci n de partida de la fuente
 
-    source() {   //Inicializo las variables de la clase.
-        p = 0.0;
-        eF = 0.0;
-        NRAYS = 0;
-        Rays = NULL;
+        source() {   //Inicializo las variables de la clase.
+            p = 0.0;
+            eF = 0.0;
+            NRAYS = 0;
+            Rays = NULL;
 
-        //create icosaedron
-        double S, R;
-        point IcoVertex[12];
+          //  color.R = 1;
+          //  Color.G = 0.5;
+         //   Color.B = 0.5;
 
-        //create vertexes
-        S = 2 / sqrt(5);
-        R = (5 - sqrt(5)) / 5;
-        IcoVertex[0].x = 0;
-        IcoVertex[0].y = 0;
-        IcoVertex[0].z = 1;
-        for (int i = 1; i < 6; i++) {
-            IcoVertex[i].x = S * cos((PI * i * 72) / 180);
-            IcoVertex[i].y = S * sin((PI * i * 72) / 180);
-            IcoVertex[i].z = 1 - R;
-            IcoVertex[i + 5].x = S * cos((72 * PI * i) / 180 + (36 * PI) / 180);
-            IcoVertex[i + 5].y = S * sin((72 * PI * i) / 180 + (36 * PI) / 180);
-            IcoVertex[i + 5].z = R - 1;
-        }
-        IcoVertex[11].x = 0;
-        IcoVertex[11].y = 0;
-        IcoVertex[11].z = -1;
+            VisualRadius = 0.3;
 
-        //create faces
-        IcoFace[0].p0 = IcoVertex[0];   IcoFace[0].p1 = IcoVertex[1];   IcoFace[0].p2 = IcoVertex[2];
-        IcoFace[1].p0 = IcoVertex[0];   IcoFace[1].p1 = IcoVertex[2];   IcoFace[1].p2 = IcoVertex[3];
-        IcoFace[2].p0 = IcoVertex[0];   IcoFace[2].p1 = IcoVertex[3];   IcoFace[2].p2 = IcoVertex[4];
-        IcoFace[3].p0 = IcoVertex[0];   IcoFace[3].p1 = IcoVertex[4];   IcoFace[3].p2 = IcoVertex[5];
-        IcoFace[4].p0 = IcoVertex[0];   IcoFace[4].p1 = IcoVertex[5];   IcoFace[4].p2 = IcoVertex[1];
-        IcoFace[5].p0 = IcoVertex[1];   IcoFace[5].p1 = IcoVertex[6];   IcoFace[5].p2 = IcoVertex[2];
-        IcoFace[6].p0 = IcoVertex[2];   IcoFace[6].p1 = IcoVertex[6];   IcoFace[6].p2 = IcoVertex[7];
-        IcoFace[7].p0 = IcoVertex[2];   IcoFace[7].p1 = IcoVertex[7];   IcoFace[7].p2 = IcoVertex[3];
-        IcoFace[8].p0 = IcoVertex[3];   IcoFace[8].p1 = IcoVertex[7];   IcoFace[8].p2 = IcoVertex[8];
-        IcoFace[9].p0 = IcoVertex[3];   IcoFace[9].p1 = IcoVertex[8];   IcoFace[9].p2 = IcoVertex[4];
-        IcoFace[10].p0 = IcoVertex[4];  IcoFace[10].p1 = IcoVertex[8];  IcoFace[10].p2 = IcoVertex[9];
-        IcoFace[11].p0 = IcoVertex[4];  IcoFace[11].p1 = IcoVertex[9];  IcoFace[11].p2 = IcoVertex[5];
-        IcoFace[12].p0 = IcoVertex[5];  IcoFace[12].p1 = IcoVertex[9];  IcoFace[12].p2 = IcoVertex[10];
-        IcoFace[13].p0 = IcoVertex[5];  IcoFace[13].p1 = IcoVertex[10]; IcoFace[13].p2 = IcoVertex[1];
-        IcoFace[14].p0 = IcoVertex[1];  IcoFace[14].p1 = IcoVertex[10]; IcoFace[14].p2 = IcoVertex[6];
-        IcoFace[15].p0 = IcoVertex[6];  IcoFace[15].p1 = IcoVertex[11]; IcoFace[15].p2 = IcoVertex[7];
-        IcoFace[16].p0 = IcoVertex[7];  IcoFace[16].p1 = IcoVertex[11]; IcoFace[16].p2 = IcoVertex[8];
-        IcoFace[17].p0 = IcoVertex[8];  IcoFace[17].p1 = IcoVertex[11]; IcoFace[17].p2 = IcoVertex[9];
-        IcoFace[18].p0 = IcoVertex[9];  IcoFace[18].p1 = IcoVertex[11]; IcoFace[18].p2 = IcoVertex[10];
-        IcoFace[19].p0 = IcoVertex[10]; IcoFace[19].p1 = IcoVertex[11]; IcoFace[19].p2 = IcoVertex[6];
-    };
+            //create icosaedron
+            double S, R;
+            point IcoVertex[12];
 
-    void createRays(double NumberOfRays) {
-        //matriz das Arestas {1o ponto da aresta, 2o ponto da aresta, Posi  o dos pontos da aresta na matriz Rays}
-        int A[30][3] = { {0,1,0}, {0,2,0}, {0,3,0}, {0,4,0}, {0,5,0},
-                        {1,6,0}, {2,6,0}, {2,7,0}, {3,7,0}, {3,8,0},
-                        {4,8,0}, {4,9,0}, {5,9,0}, {5,10,0},{1,10,0},
-                        {6,11,0},{7,11,0},{8,11,0},{9,11,0},{10,11,0},
-                        {1,2,0}, {2,3,0}, {3,4,0}, {4,5,0}, {5,1,0},
-                        {6,7,0}, {7,8,0}, {8,9,0}, {9,10,0},{10,6,0}
-        };
-        //matriz dos triangulos {1a aresta, 2a aresta, [0] V em p  [-1] V de cabe a pra baixo}
-        int T[20][3] = { {0,1,0},   {1,2,0},   {2,3,0},   {3,4,0},   {4,0,0},
-                        {5,6,-1},  {6,7,0},   {7,8,-1},  {8,9,0},   {9,10,-1},
-                        {10,11,0}, {11,12,-1},{12,13,0}, {13,14,-1},{14,5,0},
-                        {15,16,-1},{16,17,-1},{17,18,-1},{18,19,-1},{19,15,-1}
-        };
-        int i, j, k, n, m, RAY;
-        double S, R, xB, yB, zB, xC, yC, zC, c[8];
-        //create Rays matrix
-        if (NRAYS > 0)
-            delete[] Rays;
-        n = int(floor(sqrt((NumberOfRays - 2) / 10) + 0.5));
-        NRAYS = int(2 + 10 * pow(n, 2));
-        Rays = new vector1[NRAYS];
-        //calculating the icosaedron vertives
-        S = 2 / sqrt(5);
-        R = (5 - sqrt(5)) / 5;
-        Rays[0].x = 0;
-        Rays[0].y = 0;
-        Rays[0].z = 1;
-        for (i = 1; i < 6; i++) {
-            Rays[i].x = S * cos((PI * i * 72) / 180);
-            Rays[i].y = S * sin((PI * i * 72) / 180);
-            Rays[i].z = 1 - R;
-            Rays[i + 5].x = S * cos((72 * PI * i) / 180 + (36 * PI) / 180);
-            Rays[i + 5].y = S * sin((72 * PI * i) / 180 + (36 * PI) / 180);
-            Rays[i + 5].z = R - 1;
-        }
-        Rays[11].x = 0;
-        Rays[11].y = 0;
-        Rays[11].z = -1;
-        RAY = 12;
-        //calculating the rays on the icosaedron edges
-        for (j = 0; j < 30; j++) {
-            A[j][2] = RAY;
-            xB = Rays[A[j][0]].x;
-            yB = Rays[A[j][0]].y;
-            zB = Rays[A[j][0]].z;
-            xC = Rays[A[j][1]].x;
-            yC = Rays[A[j][1]].y;
-            zC = Rays[A[j][1]].z;
-            c[0] = pow(xC, 2) * (pow(yB, 2) + pow(zB, 2)) + pow(yC * zB - yB * zC, 2) - 2 * xB * xC * (yB * yC + zB * zC) + pow(xB, 2) * (pow(yC, 2) + pow(zC, 2));
-            c[1] = acos(xB * xC + yB * yC + zB * zC);
-            c[2] = -xC * (yB * yC + zB * zC) + xB * (pow(yC, 2) + pow(zC, 2));
-            c[3] = xC * (pow(yB, 2) + pow(zB, 2)) - xB * (yB * yC + zB * zC);
-            c[4] = pow(xC, 2) * yB - xB * xC * yC + zC * (-yC * zB + yB * zC);
-            c[5] = -xB * xC * yB + pow(xB, 2) * yC + zB * (yC * zB - yB * zC);
-            c[6] = pow(xC, 2) * zB - xB * xC * zC + yC * (yC * zB - yB * zC);
-            c[7] = -xB * xC * zB + pow(xB, 2) * zC + yB * (-yC * zB + yB * zC);
-            for (i = 1; i < n; i++) {
-                Rays[RAY].x = (c[2] * cos(i * c[1] / n) + c[3] * cos((n - i) * c[1] / n)) / c[0];
-                Rays[RAY].y = (c[4] * cos(i * c[1] / n) + c[5] * cos((n - i) * c[1] / n)) / c[0];
-                Rays[RAY].z = (c[6] * cos(i * c[1] / n) + c[7] * cos((n - i) * c[1] / n)) / c[0];
-                RAY++;
+            //create vertexes
+            S = 2 / sqrt(5);
+            R = (5 - sqrt(5)) / 5;
+            IcoVertex[0].x = 0;
+            IcoVertex[0].y = 0;
+            IcoVertex[0].z = 1;
+            for (int i = 1; i < 6; i++) {
+                IcoVertex[i].x = S * cos((PI * i * 72) / 180);
+                IcoVertex[i].y = S * sin((PI * i * 72) / 180);
+                IcoVertex[i].z = 1 - R;
+                IcoVertex[i + 5].x = S * cos((72 * PI * i) / 180 + (36 * PI) / 180);
+                IcoVertex[i + 5].y = S * sin((72 * PI * i) / 180 + (36 * PI) / 180);
+                IcoVertex[i + 5].z = R - 1;
             }
-        }
-        //calculating the rays on the icosaedron faces
-        for (k = 0; k < 20; k++)
-            for (j = 1; j < n; j++) {
-                xB = Rays[A[T[k][0]][2] + j - 1].x;
-                yB = Rays[A[T[k][0]][2] + j - 1].y;
-                zB = Rays[A[T[k][0]][2] + j - 1].z;
-                xC = Rays[A[T[k][1]][2] + j - 1].x;
-                yC = Rays[A[T[k][1]][2] + j - 1].y;
-                zC = Rays[A[T[k][1]][2] + j - 1].z;
+            IcoVertex[11].x = 0;
+            IcoVertex[11].y = 0;
+            IcoVertex[11].z = -1;
+
+            //create faces
+            IcoFace[0].p0 = IcoVertex[0];   IcoFace[0].p1 = IcoVertex[1];   IcoFace[0].p2 = IcoVertex[2];
+            IcoFace[1].p0 = IcoVertex[0];   IcoFace[1].p1 = IcoVertex[2];   IcoFace[1].p2 = IcoVertex[3];
+            IcoFace[2].p0 = IcoVertex[0];   IcoFace[2].p1 = IcoVertex[3];   IcoFace[2].p2 = IcoVertex[4];
+            IcoFace[3].p0 = IcoVertex[0];   IcoFace[3].p1 = IcoVertex[4];   IcoFace[3].p2 = IcoVertex[5];
+            IcoFace[4].p0 = IcoVertex[0];   IcoFace[4].p1 = IcoVertex[5];   IcoFace[4].p2 = IcoVertex[1];
+            IcoFace[5].p0 = IcoVertex[1];   IcoFace[5].p1 = IcoVertex[6];   IcoFace[5].p2 = IcoVertex[2];
+            IcoFace[6].p0 = IcoVertex[2];   IcoFace[6].p1 = IcoVertex[6];   IcoFace[6].p2 = IcoVertex[7];
+            IcoFace[7].p0 = IcoVertex[2];   IcoFace[7].p1 = IcoVertex[7];   IcoFace[7].p2 = IcoVertex[3];
+            IcoFace[8].p0 = IcoVertex[3];   IcoFace[8].p1 = IcoVertex[7];   IcoFace[8].p2 = IcoVertex[8];
+            IcoFace[9].p0 = IcoVertex[3];   IcoFace[9].p1 = IcoVertex[8];   IcoFace[9].p2 = IcoVertex[4];
+            IcoFace[10].p0 = IcoVertex[4];  IcoFace[10].p1 = IcoVertex[8];  IcoFace[10].p2 = IcoVertex[9];
+            IcoFace[11].p0 = IcoVertex[4];  IcoFace[11].p1 = IcoVertex[9];  IcoFace[11].p2 = IcoVertex[5];
+            IcoFace[12].p0 = IcoVertex[5];  IcoFace[12].p1 = IcoVertex[9];  IcoFace[12].p2 = IcoVertex[10];
+            IcoFace[13].p0 = IcoVertex[5];  IcoFace[13].p1 = IcoVertex[10]; IcoFace[13].p2 = IcoVertex[1];
+            IcoFace[14].p0 = IcoVertex[1];  IcoFace[14].p1 = IcoVertex[10]; IcoFace[14].p2 = IcoVertex[6];
+            IcoFace[15].p0 = IcoVertex[6];  IcoFace[15].p1 = IcoVertex[11]; IcoFace[15].p2 = IcoVertex[7];
+            IcoFace[16].p0 = IcoVertex[7];  IcoFace[16].p1 = IcoVertex[11]; IcoFace[16].p2 = IcoVertex[8];
+            IcoFace[17].p0 = IcoVertex[8];  IcoFace[17].p1 = IcoVertex[11]; IcoFace[17].p2 = IcoVertex[9];
+            IcoFace[18].p0 = IcoVertex[9];  IcoFace[18].p1 = IcoVertex[11]; IcoFace[18].p2 = IcoVertex[10];
+            IcoFace[19].p0 = IcoVertex[10]; IcoFace[19].p1 = IcoVertex[11]; IcoFace[19].p2 = IcoVertex[6];
+        };
+
+        void createRays(double NumberOfRays) {
+            //matriz das Arestas {1o ponto da aresta, 2o ponto da aresta, Posi��o dos pontos da aresta na matriz Rays}
+            int A[30][3] = { {0,1,0}, {0,2,0}, {0,3,0}, {0,4,0}, {0,5,0},
+                            {1,6,0}, {2,6,0}, {2,7,0}, {3,7,0}, {3,8,0},
+                            {4,8,0}, {4,9,0}, {5,9,0}, {5,10,0},{1,10,0},
+                            {6,11,0},{7,11,0},{8,11,0},{9,11,0},{10,11,0},
+                            {1,2,0}, {2,3,0}, {3,4,0}, {4,5,0}, {5,1,0},
+                            {6,7,0}, {7,8,0}, {8,9,0}, {9,10,0},{10,6,0}
+            };
+            //matriz dos triangulos {1a aresta, 2a aresta, [0] V em p� [-1] V de cabe�a pra baixo}
+            int T[20][3] = { {0,1,0},   {1,2,0},   {2,3,0},   {3,4,0},   {4,0,0},
+                            {5,6,-1},  {6,7,0},   {7,8,-1},  {8,9,0},   {9,10,-1},
+                            {10,11,0}, {11,12,-1},{12,13,0}, {13,14,-1},{14,5,0},
+                            {15,16,-1},{16,17,-1},{17,18,-1},{18,19,-1},{19,15,-1}
+            };
+            int i, j, k, n, m, RAY;
+            double S, R, xB, yB, zB, xC, yC, zC, c[8];
+            //create Rays matrix
+            if (NRAYS > 0)
+                delete[] Rays;
+            n = int(floor(sqrt((NumberOfRays - 2) / 10) + 0.5));
+            NRAYS = int(2 + 10 * pow(n, 2));
+            Rays = new vector1[NRAYS];
+            //calculating the icosaedron vertives
+            S = 2 / sqrt(5);
+            R = (5 - sqrt(5)) / 5;
+            Rays[0].x = 0;
+            Rays[0].y = 0;
+            Rays[0].z = 1;
+            for (i = 1; i < 6; i++) {
+                Rays[i].x = S * cos((PI * i * 72) / 180);
+                Rays[i].y = S * sin((PI * i * 72) / 180);
+                Rays[i].z = 1 - R;
+                Rays[i + 5].x = S * cos((72 * PI * i) / 180 + (36 * PI) / 180);
+                Rays[i + 5].y = S * sin((72 * PI * i) / 180 + (36 * PI) / 180);
+                Rays[i + 5].z = R - 1;
+            }
+            Rays[11].x = 0;
+            Rays[11].y = 0;
+            Rays[11].z = -1;
+            RAY = 12;
+            //calculating the rays on the icosaedron edges
+            for (j = 0; j < 30; j++) {
+                A[j][2] = RAY;
+                xB = Rays[A[j][0]].x;
+                yB = Rays[A[j][0]].y;
+                zB = Rays[A[j][0]].z;
+                xC = Rays[A[j][1]].x;
+                yC = Rays[A[j][1]].y;
+                zC = Rays[A[j][1]].z;
                 c[0] = pow(xC, 2) * (pow(yB, 2) + pow(zB, 2)) + pow(yC * zB - yB * zC, 2) - 2 * xB * xC * (yB * yC + zB * zC) + pow(xB, 2) * (pow(yC, 2) + pow(zC, 2));
                 c[1] = acos(xB * xC + yB * yC + zB * zC);
                 c[2] = -xC * (yB * yC + zB * zC) + xB * (pow(yC, 2) + pow(zC, 2));
@@ -785,16 +768,41 @@ public:
                 c[5] = -xB * xC * yB + pow(xB, 2) * yC + zB * (yC * zB - yB * zC);
                 c[6] = pow(xC, 2) * zB - xB * xC * zC + yC * (yC * zB - yB * zC);
                 c[7] = -xB * xC * zB + pow(xB, 2) * zC + yB * (-yC * zB + yB * zC);
-                if (T[k][2] == 0)m = j;
-                else m = n - j;
-                for (i = 1; i < m; i++) {
-                    Rays[RAY].x = (c[2] * cos(i * c[1] / m) + c[3] * cos((m - i) * c[1] / m)) / c[0];
-                    Rays[RAY].y = (c[4] * cos(i * c[1] / m) + c[5] * cos((m - i) * c[1] / m)) / c[0];
-                    Rays[RAY].z = (c[6] * cos(i * c[1] / m) + c[7] * cos((m - i) * c[1] / m)) / c[0];
+                for (i = 1; i < n; i++) {
+                    Rays[RAY].x = (c[2] * cos(i * c[1] / n) + c[3] * cos((n - i) * c[1] / n)) / c[0];
+                    Rays[RAY].y = (c[4] * cos(i * c[1] / n) + c[5] * cos((n - i) * c[1] / n)) / c[0];
+                    Rays[RAY].z = (c[6] * cos(i * c[1] / n) + c[7] * cos((n - i) * c[1] / n)) / c[0];
                     RAY++;
                 }
             }
-    };
+            //calculating the rays on the icosaedron faces
+            for (k = 0; k < 20; k++)
+                for (j = 1; j < n; j++) {
+                    xB = Rays[A[T[k][0]][2] + j - 1].x;
+                    yB = Rays[A[T[k][0]][2] + j - 1].y;
+                    zB = Rays[A[T[k][0]][2] + j - 1].z;
+                    xC = Rays[A[T[k][1]][2] + j - 1].x;
+                    yC = Rays[A[T[k][1]][2] + j - 1].y;
+                    zC = Rays[A[T[k][1]][2] + j - 1].z;
+                    c[0] = pow(xC, 2) * (pow(yB, 2) + pow(zB, 2)) + pow(yC * zB - yB * zC, 2) - 2 * xB * xC * (yB * yC + zB * zC) + pow(xB, 2) * (pow(yC, 2) + pow(zC, 2));
+                    c[1] = acos(xB * xC + yB * yC + zB * zC);
+                    c[2] = -xC * (yB * yC + zB * zC) + xB * (pow(yC, 2) + pow(zC, 2));
+                    c[3] = xC * (pow(yB, 2) + pow(zB, 2)) - xB * (yB * yC + zB * zC);
+                    c[4] = pow(xC, 2) * yB - xB * xC * yC + zC * (-yC * zB + yB * zC);
+                    c[5] = -xB * xC * yB + pow(xB, 2) * yC + zB * (yC * zB - yB * zC);
+                    c[6] = pow(xC, 2) * zB - xB * xC * zC + yC * (yC * zB - yB * zC);
+                    c[7] = -xB * xC * zB + pow(xB, 2) * zC + yB * (-yC * zB + yB * zC);
+                    if (T[k][2] == 0)m = j;
+                    else m = n - j;
+                    for (i = 1; i < m; i++) {
+                        Rays[RAY].x = (c[2] * cos(i * c[1] / m) + c[3] * cos((m - i) * c[1] / m)) / c[0];
+                        Rays[RAY].y = (c[4] * cos(i * c[1] / m) + c[5] * cos((m - i) * c[1] / m)) / c[0];
+                        Rays[RAY].z = (c[6] * cos(i * c[1] / m) + c[7] * cos((m - i) * c[1] / m)) / c[0];
+                        RAY++;
+                    }
+                }
+        };
+
 };
 //---------------------------------------------------------------------------
 class MatDouble {
@@ -850,10 +858,6 @@ public:
 
         return Color(r, g, b);
     }
-
-
-
-
 };
 
 //---------------------------------------------------------------------------
