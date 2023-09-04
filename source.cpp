eb6e97487f8f0df21d67a426a4e544b3c04e42e8
Adrian Laje <67974071+adrianrd7@users.noreply.github.com>
2023-09-04 16:15:57 -0500

    Update source.cpp

--- source.cpp
+++ source.cpp
@@ -17,33 +17,30 @@
 bool loadedRoom; //si la habitación ha sido cargada .
 room r;          // Instancia para representar la habitación. 
 MatDouble mD; //Matriz que almacene la distancia que existe entre los centros de los diferentes triángulos de la sala.
-             //Está matriz tiene una dimensión de 𝑛 × 𝑛 donde 𝑛 es el número de triángulos (NumTri) de la sala.
+//Está matriz tiene una dimensión de 𝑛 × 𝑛 donde 𝑛 es el número de triángulos (NumTri) de la sala.
 MatDouble mTV;//matriz que almacena los tiempos de vuelo entre baricentros en milisegundos que demorarían las reflexiones difusas
-              //para llegar de un centroide a otro(entre todos los triángulos que apliquen, es decir que sean visibles)
-              // La matriz tendrá una dimensión de 𝑛 × n
+//para llegar de un centroide a otro(entre todos los triángulos que apliquen, es decir que sean visibles)
+// La matriz tendrá una dimensión de 𝑛 × n
 MatDouble mAS; //matriz angulos solidos
 MatDouble mPE; //matriz porcentage de energia. La matriz tendrá una dimensión de 𝑛 × n
 MatDouble mE; //matriz porcentage de energia. (espacio/tiempo) 
 int NumTri = 0; //Almacena el número total de triángulos.
-int N_RAYOS = 20; //Almacena el numero de rayos
+double N_RAYOS = 20; //Almacena el numero de rayos
 int energiaFuente = 100; //Energia de la fuente
 source s;           // Instancia de la fuente.
-float speed = 0.3; //Velocidad de movimiento de la animacion
 float alfa = 0.2; //Defincion del coeficiente alfa
 float delta = 0.15; //Defincion del coeficiente alfa
 int tM = 1000; //tiempo de simulacion en unidad discreta
 double v_son = 340.0; //velocidad de transmisión de 340 m/s (constante V_SON)
 reflection* arrayreflecciones = NULL; //Refleciones
-point o ; //Punto de origen
-float simulationInterval = 0.00625f;
-point** arrayRec;
-
-
+point o; //Punto de origen
+point** arrayRec;  //Array Recorrido
+float elapsedTime = 0.0f;
 
 void laodRoom();
 void energytransition();
 
-float elapsedTime = 0.0f;
+
 
 //General Settings
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -55,7 +52,7 @@ void processInput(GLFWwindow* window);
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 800;
 // camera
-Camera camera(glm::vec3(0.0f, 0.0f, 3.0f));
+Camera camera(glm::vec3(0.0f, 0.0f, 10.0f));
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
@@ -111,7 +108,6 @@ int main()
     float vertices1[108];
     int contradork = 0;
 
-
     //El vertex imput para el cubo y la particula
     for (int i = 0; i < r.NP; i++) {
         for (int j = 0; j < r.p[i].NT; j++) {
@@ -121,7 +117,7 @@ int main()
             contradork++;
             vertices1[contradork] = r.p[i].t[j].p0.z;
             contradork++;
-            vertices1[contradork] = r.p[i].t[j].p1.x ;
+            vertices1[contradork] = r.p[i].t[j].p1.x;
             contradork++;
             vertices1[contradork] = r.p[i].t[j].p1.y;
             contradork++;
@@ -136,12 +132,10 @@ int main()
 
         }
     }
-  
-
 
     energytransition();
 
-  
+
     //Configuracion del Ambiente para el cubo y la particula
     // first, configure the cube's VAO (and VBO)
     unsigned int VBO, cubeVAO;
@@ -153,7 +147,7 @@ int main()
     // position attribute
     glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
-   
+
     //Configuracion del cubo de luz
     unsigned int lightCubeVBO, lightCubeVAO;
     glGenVertexArrays(1, &lightCubeVAO);
@@ -177,7 +171,7 @@ int main()
     }
     int* sumImpactosPorTriangulo = new int[NumTri]();
     // Difusión de la energía en los triángulos
-    for (int r = 0; r < N_RAYOS; r++) {
+    for (int r = 0; r < s.NRAYS; r++) {
         double distAcum = 0;
         int tiempo = 0;
         for (int re = 0; re < 50; re++) {
@@ -192,12 +186,7 @@ int main()
 
     for (int i = 0; i < NumTri; i++) {
         for (int j = 1; j < tM; j++) {
-            if (i == 0) {
-                impactosPorTriangulo[i][j] = (impactosPorTriangulo[i][j] / 3) + impactosPorTriangulo[i][j - 1];
-            }
-            else {
-                impactosPorTriangulo[i][j] = impactosPorTriangulo[i][j] + impactosPorTriangulo[i][j - 1];
-            }
+            impactosPorTriangulo[i][j] = impactosPorTriangulo[i][j] + impactosPorTriangulo[i][j - 1];
             maxImpTri[i] = impactosPorTriangulo[i][j];
         }
     }
@@ -247,24 +236,21 @@ int main()
 
         for (int i = 0; i < NumTri; i++) {
             // Configura el color calculado en el shader
-            
+
             // Obtén el color calculado en función del número de impactos y el valor máximo de impactos
 
             Color triangleColor = Color::heatMapColor(impactosPorTriangulo[i][tSimulacion], maxImpTri[i]);
 
-
             // Configura el color calculado en el shader
             room.setVec3("ourColor", glm::vec3(triangleColor.red, triangleColor.green, triangleColor.blue));
-          
             room.setMat4("projection", projection);
             room.setMat4("view", view);
-           
             room.setMat4("model", model);
             // render the cube
             glBindVertexArray(cubeVAO);
-            glDrawArrays(GL_TRIANGLES, i * 3, 3); 
+            glDrawArrays(GL_TRIANGLES, i * 3, 3);
         }
-       
+
         //Dibujar la fuente
         rayo.use();
         rayo.setMat4("projection", projection);
@@ -272,16 +258,13 @@ int main()
         rayo.setVec3("ourColor", 0.5f, 1.0f, 0.5f);
         model = glm::mat4(1.0f);
 
-
-
-        for (int idRayo = 0; idRayo < 20; idRayo ++ ) {
+        for (int idRayo = 0; idRayo < s.NRAYS; idRayo++) {
             // Calculate the model matrix for each ray instance
             glm::mat4 model = glm::mat4(1.0f);
-
             model = glm::translate(model, glm::vec3(arrayRec[idRayo][tSimulacion].x,
                 arrayRec[idRayo][tSimulacion].y,
                 arrayRec[idRayo][tSimulacion].z));
-            
+
             model = glm::scale(model, glm::vec3(0.025f)); // Tamaño del cubo pequeño
             rayo.setMat4("model", model);
 
@@ -290,8 +273,7 @@ int main()
             glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
             glDrawArrays(GL_TRIANGLES, 0, 36);
         }
-        
-        
+
         glfwSwapBuffers(window);
         glfwPollEvents();
 
@@ -451,7 +433,7 @@ void laodRoom() {
         r.p[5].p[3].x = 2.0f;
         r.p[5].p[3].y = 2.0f;
         r.p[5].p[3].z = -2.0f;
-        r.p[5].PointGenTriangle(); 
+        r.p[5].PointGenTriangle();
 
         int cont_t = 0; //contador del numero de triangulos
         // Loop para calcular el centroide de todos los triángulos de la sala
@@ -483,7 +465,7 @@ void laodRoom() {
                             mD.A[idTri1][idTri2] = r.p[i].t[j].bc.distancia(r.p[k].t[l].bc); //Calculo de la distancia de baricentro de los triangulos de la sala hacia otro triangulo de la sala NO coplanar
                             mTV.A[idTri1][idTri2] = int(1000 * mD.A[idTri1][idTri2] / V_SON); // Calculo de la distancia de baricentro de los triangulos de la sala hacia otro triangulo de la sala NO coplanar
                             mAS.A[idTri1][idTri2] = r.p[k].t[l].AnguloSolido(r.p[i].t[j].bc); // Calculo de la distancia de baricentro de los triangulos de la sala hacia otro triangulo de la sala NO coplanar
-                            suma_angulos_solidos[cont] += abs(mAS.A[idTri1][idTri2]); 
+                            suma_angulos_solidos[cont] += abs(mAS.A[idTri1][idTri2]);
                         }
                     }
                 }
@@ -524,74 +506,54 @@ void laodRoom() {
         delete[] suma_angulos_solidos;
 
         loadedRoom = true; // Indica que la habitación ha sido cargada exitosamente.
-
     }
 }
 void energytransition() {
     if (loadedRoom) {
         int t_vuelo = 0; // Tiempo de vuelo del rayo
 
-
         o.x = 1.0;
         o.y = 1.0;
         o.z = 1.0;
 
         s.eF = energiaFuente; //Energia de la fuente
+
         s.createRays(N_RAYOS);  //Numero de rayos
 
-        double eneRayo = s.eF / N_RAYOS; // Energía Inicial
+        double eneRayo = s.eF / s.NRAYS; // Energía Inicial
 
-
-        /*
-        for (int i = 0; i < 20; i++) { 
-            printf("Rayos: x: %f, y: %f, z: %f\n", s.Rays[i].x, s.Rays[i].y, s.Rays[i].z);
-        }
-        */
-
-        arrayreflecciones = r.RayTracing(o, s.Rays, N_RAYOS);
-        /*
-        for (int i = 0; i < 50; i++) {
-            printf(" %d - Punto de Incidencia: x: %f, y: %f, z: %f\n", i, arrayreflecciones[1].r[i].x, arrayreflecciones[1].r[i].y, arrayreflecciones[1].r[i].z);
-        }
-        */
-
+        arrayreflecciones = r.RayTracing(o, s.Rays, s.NRAYS);
 
         mE.Init(NumTri, tM); //inicializacion de la matriz de energia
 
+        arrayRec = new point * [s.NRAYS];
 
-        arrayRec = new point * [N_RAYOS];
-
-        // Difusión de la energía en los triángulos
-        for (int r = 0; r < N_RAYOS; r++) {
+        // Difusión de la energía difusa en los triángulos
+        for (int r = 0; r < s.NRAYS; r++) {
             double eneResidual = eneRayo;
             double distAcum = 0;
             arrayRec[r] = new point[tM];
-
             arrayRec[r][0] = o;
             int tiempo = 0;
-            for (int re = 0; re < 50; re++) {
+            for (int re = 0; re < arrayreflecciones[r].N; re++) {
                 int tri = arrayreflecciones[r].idTriangle[re];
                 point pun = arrayreflecciones[r].r[re];
                 distAcum += arrayreflecciones[r].d[re];
-
                 int tim = int(1000 * distAcum / v_son);
                 arrayRec[r][tim] = pun;
-
                 for (int t = tiempo + 1; t < tim; t++) {
                     arrayRec[r][t].x = arrayRec[r][t - 1].x + (arrayRec[r][tim].x - arrayRec[r][tiempo].x) / (tim - tiempo);
                     arrayRec[r][t].y = arrayRec[r][t - 1].y + (arrayRec[r][tim].y - arrayRec[r][tiempo].y) / (tim - tiempo);
                     arrayRec[r][t].z = arrayRec[r][t - 1].z + (arrayRec[r][tim].z - arrayRec[r][tiempo].z) / (tim - tiempo);
                 }
-
                 tiempo = tim;
-                mE.A[tri][tim] += (eneResidual * (1 - alfa) * delta);
-
-                eneResidual = eneResidual * (1 - alfa) * (1 - delta);
+                mE.A[tri][tim] += (eneResidual * (1 - alfa) * delta); //Energia difusa en los triangulos
+                eneResidual = eneResidual * (1 - alfa) * (1 - delta); //Energia incidente de los rayos
             }
         }
 
 
-        //Transicion de energia
+        //Transicion de energia en la matriz esapcio tiempo mE
         for (int t = 0; t < tM; t++) {
             for (int e = 0; e < NumTri; e++) {// Triángulo 1
                 for (int ed = 0; ed < NumTri; ed++) { // Triángulo 2
@@ -599,11 +561,8 @@ void energytransition() {
                         //Es el instante de tiempo de la simulacion + el instate de  tiempo en que ocurre la transmision de energia en ese triangulo a hacia el siguiente triangulo
                         t_vuelo = mTV.A[e][ed] + t;
                         if (t_vuelo <= tM) {
-
                             mE.A[ed][t_vuelo] += (mE.A[e][t] * mPE.A[e][ed]) * (1 - alfa);
                         }
-                        
-                        
                     }
                 }
             }
